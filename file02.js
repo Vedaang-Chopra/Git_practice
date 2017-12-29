3f10f3528c523815ac22917eb2e130298fda3d15
Vedaang Chopra <vedaangchopra@hotmail.com>
2017-12-29 21:32:55 +0530

    Changes in gitlearning branch

--- file02.js
+++ file02.js
@@ -1,2 +1,5 @@
 console.log("Hey we are learning git");
 console.log("Yo man");
+console.log("Use git status to check current status");
+console.log("Use git log to checks history");
+console.log("Only in git learning")
